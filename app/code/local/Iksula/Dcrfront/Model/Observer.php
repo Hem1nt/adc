ece16ddee7a96e2bedef88186708f4bf308b21cc
hemant-r <hemant.r@iksula.com>
2017-08-28 12:57:48 +0530

    updated pay link var1

--- app/code/local/Iksula/Dcrfront/Model/Observer.php
+++ app/code/local/Iksula/Dcrfront/Model/Observer.php
@@ -22,14 +22,14 @@ class Iksula_Dcrfront_Model_Observer
                 // Set sender information            
                 $senderName = Mage::getStoreConfig('trans_email/ident_support/name');
                 $senderEmail = Mage::getStoreConfig('trans_email/ident_support/email');        
-                $sender = array('pay_link'=>$payNowLink,'name' => $senderName,'email' => $senderEmail);    
+                $sender = array('name' => $senderName,'email' => $senderEmail);    
                 // Set recepient information
                 $recepientEmail = $customerPlaced;
                 $recepientName = $customerFirstname;  
                 // Get Store ID        
                 $storeId = Mage::app()->getStore()->getId();
                 // Set variables that can be used in email template
-                $vars = array('customer_name' => $customerFirstname);
+                $vars = array('pay_link'=>$payNowLink,'customer_name' => $customerFirstname);
                 $translate  = Mage::getSingleton('core/translate');
                 // Send Transactional Email
                 Mage::getModel('core/email_template')
