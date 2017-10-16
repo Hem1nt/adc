a9d52a45b0119163739f5f6b10b4407170e8fe4d
prashant_iksula033 <prashant.g@iksula.com>
2017-10-16 17:38:38 +0530

    extended review render code

--- app/code/local/Iksula/ExtendedReview/Block/Adminhtml/Extendedreview/Renderer/Comment.php
+++ app/code/local/Iksula/ExtendedReview/Block/Adminhtml/Extendedreview/Renderer/Comment.php
@@ -3,24 +3,27 @@
 Class Iksula_ExtendedReview_Block_Adminhtml_Extendedreview_Renderer_Comment extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
     public function render(Varien_Object $row){
-    	
-    	$reviewId =  $row->getReviewId();
+    
+     	$reviewId =  $row->getReviewId();
+      $review = Mage::getModel('review/review')->load($reviewId);
     	$commentId= $row->getComentId();
+      $Comment_detail=$review->getDetail();
     	$customer_id=$row->getCustomerId();
-    	//print_r($customer_id);exit;
+    	
   
         $commentCollection=Mage::getModel("extendedreview/extendedreview")->getCollection()
-       // ->addFieldToFilter('review_id',$reviewId)
+        ->addFieldToSelect('*')
         ->addFieldToFilter('review_id',array('in' => array($reviewId)))
-        // ->addFieldToFilter('comment_id',array('in' => array($commentId)))
         ->addFieldToFilter('customer_id',$customer_id)
-         					//->addFieldToFilter('id',$id);
         ;
-        $count=0;
+        $count = 1;
+        echo "<p>".'Actual Review :'.'<br/>'.$Comment_detail."</p>";
        foreach ($commentCollection as $value) {
-       	$count++;
-       	echo $count;
-       	echo $value->getComment()."<br>";
+          //$count++;
+        echo "<br/>";
+        echo "Comment ".$count.":";
+        echo '<strong style="margin:0 6px 16px 44px;">'.$value->getComment().'</strong>';
+        $count++; 
        }
 
         
