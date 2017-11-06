1a3819d58f805755f06435be2c0744372abfa60a
prashant_iksula033 <prashant.g@iksula.com>
2017-11-06 14:33:09 +0530

    comment and review by if condition

--- app/code/local/Iksula/ExtendedReview/Block/Adminhtml/Extendedreview/Renderer/Comment.php
+++ app/code/local/Iksula/ExtendedReview/Block/Adminhtml/Extendedreview/Renderer/Comment.php
@@ -5,7 +5,6 @@ Class Iksula_ExtendedReview_Block_Adminhtml_Extendedreview_Renderer_Comment exte
     public function render(Varien_Object $row){
 
       $id=$row->getId();
-       //print_r($Id);exit;
      	$reviewId =  $row->getReviewId();
       $review = Mage::getModel('review/review')->load($reviewId);
     	$commentId= $row->getComentId();
@@ -15,23 +14,42 @@ Class Iksula_ExtendedReview_Block_Adminhtml_Extendedreview_Renderer_Comment exte
   
         $commentCollection=Mage::getModel("extendedreview/extendedreview")->getCollection()
         ->addFieldToSelect('*')
-        ->addFieldToFilter('id',array('in' => array($id)))
-        //->addFieldToFilter('comment_id',$commentId)
+        //->addFieldToFilter('id',array('in' => array($id)))
+        ->addFieldToFilter('review_id',$reviewId)
+        //->addFieldToFilter('comment_id','0')
        // ->addFieldToFilter('comment_id',array('in' => array($commentId)))
         ->addFieldToFilter('customer_id',$customer_id)
         ;
-        //$count = 1;
+   
+
+
+        $count = '1';
         echo "<p><i>".'Actual Review :'.'<br/>'.$Comment_detail."</i></p>";
 
-       foreach ($commentCollection as $value) {
 
-        echo "<br/>";
+       foreach ($commentCollection as $value) {
+        $comment_id=$value['comment_id'];
+        //print_r($comment_id);exit;
+        if($comment_id=='0')
+        {
+           echo "<br/>";
         echo "Comment ".$count.":";
+        
         echo '<strong style="margin:0 6px 16px 18px;">'.$value->getComment().'</strong>';
         $count++; 
+        }
+        else
+        {
+           echo "<br/>";
+        echo "Sub Comment ".$count.":";
+
+        // echo '<strong style="margin:0 6px 16px 18px;">'.$value->getComment().'</strong>';
+        echo '<strong style="margin:0 6px 20px 27px; color:#ea7601 !important;">'.$value->getComment().'</strong>';
+        $count++;
+        }
+       
        
        }
-
         
     }
 }
