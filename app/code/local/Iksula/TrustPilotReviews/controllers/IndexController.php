4ea86b59fcf0fdd41faf55f98564626fa89f3751
ankitIksula <ankit.a@iksulaops.com>
2017-11-10 15:21:56 +0530

    trustpilot changes

--- app/code/local/Iksula/TrustPilotReviews/controllers/IndexController.php
+++ app/code/local/Iksula/TrustPilotReviews/controllers/IndexController.php
@@ -43,17 +43,17 @@ class Iksula_TrustPilotReviews_IndexController extends Mage_Core_Controller_Fron
             $html .=  '<div class="review_img"><image src="http://images-static.trustpilot.com/api/stars/'.$reviews['stars'].'/130x24.png" ></div>'; 
             $html .=  '<div class="consumer_name">'.$reviews['consumer']['displayName'].'</div>';
             $timpestamp =  strtotime($reviews['createdAt']);
-            $html .=  "<div class='review_date'>".date('Y-m-d',$timpestamp)."</div>";
+            $html .=  "<div class='review_date'>".date('M d, Y',$timpestamp)."</div>";
 
             
             $html .=  '</div>';
             if($reviews['companyReply']['text'] != ''):$class = 'review_wrap_highlight';endif;
             $html .=  '<div class="review_wrap '.$class.'">';
             $html .=  "<div class='review_title'>".$reviews['title'].'</div>';
-            $html .=  "<div class='review_txt'>".$reviews['text'].'</div>';
+            $html .=  "<div class='review_txt'>".ucfirst(strtolower($reviews['text'])).'</div>';
             $html .=  '</div>';
             if($reviews['companyReply']['text'] != ''):$html .=  "<div class='companyReply'><div style='color:red;font-weight: normal;
-            color: #506f00;font-weight: 600;'>Company Reply </div><br>".$reviews['companyReply']['text'].'</div>';endif;
+            color: #506f00;font-weight: 600;'>AllDayChemist Reply </div><br>".ucfirst(strtolower($reviews['companyReply']['text'])).'</div>';endif;
             $html .=  '</div>';
           }
           echo json_encode(array('success'=>'true','apiurl'=>$url,'data'=>$html));
