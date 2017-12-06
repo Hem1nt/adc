53487fa8f9aea3fa7b9ea8fd753f1d488ddb0a47
ankitIksula <ankit.a@iksulaops.com>
2017-12-06 14:23:48 +0530

    count orders changes based on status

--- app/code/local/Iksula/Overrides/Model/Observer.php
+++ app/code/local/Iksula/Overrides/Model/Observer.php
@@ -816,9 +816,9 @@ public function customerOrderGroupChange($observer){
     $customerId = $order->getCustomerId();
     $customerEmail = $order->getCustomerEmail();
     $orderCollection = Mage::getModel('sales/order')->getCollection()->addFieldToFilter('customer_email',$customerEmail);
-    //$orderHelper = Mage::helper('frontend/order');
-    //$customersOrdersCount = $orderHelper->getCustomersOrdersCount($customerEmail);
-    $customersOrdersCount = count($orderCollection->getData());
+    $orderHelper = Mage::helper('frontend/order');
+    $customersOrdersCount = $orderHelper->getCustomersOrdersCount($customerEmail);
+    //$customersOrdersCount = count($orderCollection->getData());
     if($customerId){
         if($customersOrdersCount >= 3){
             foreach ($orderCollection as $_order) {
