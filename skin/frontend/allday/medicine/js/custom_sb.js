fb98f0c02c5101768f3e4791a4549d3860d44da6
sonal <sonal.b@iksula.com>
2017-12-20 16:21:23 +0530

    adc christmas theme done

--- skin/frontend/allday/medicine/js/custom_sb.js
+++ skin/frontend/allday/medicine/js/custom_sb.js
@@ -120,11 +120,16 @@ jQuery(document).ready(function($){
    });
  } 
  
- equalheight('ul.products-grid li.item');
-
- $(window).on('resize', function(){
    equalheight('ul.products-grid li.item');
- });
 
+   $(window).on('resize', function(){
+     equalheight('ul.products-grid li.item');
+   });
+
+
+   $("body").addClass("adc_christmas");
+
+       jQuery.fn.snow({minSize: 15, maxSize: 25, newOn: 500, flakeColor: '#bbb' });/*minSize: 5, maxSize: 25, newOn: 1000, */
+       jQuery.fn.snow1({minSize: 10, maxSize: 20, newOn: 400, flakeColor: '#bbb' });/*minSize: 5, maxSize: 25, newOn: 1000, */
 
   });
