b3d96cc0f2976c58f671d4ba38462e9d1e7c2fbe
ankitIksula <ankit.a@iksulaops.com>
2018-01-19 20:27:21 +0530

    install and upgrade file

---
+++ app/code/local/Iksula/Overrides/sql/overrides_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -0,0 +1,18 @@
+<?php
+$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
+/**
+ * Add 'custom_attribute' attribute for entities
+ */
+$entities = array(
+    'quote',
+    'order'
+);
+$options = array(
+    'type'     => Varien_Db_Ddl_Table::TYPE_VARCHAR,
+    'visible'  => true,
+    'required' => false
+);
+foreach ($entities as $entity) {
+    $installer->addAttribute($entity, 'find_us_other', $options);
+}
+$installer->endSetup();
