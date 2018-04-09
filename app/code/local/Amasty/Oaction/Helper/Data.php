2b54c4e55ea487528861a2f66741f87150ff1f66
ankitIksula <ankit.a@iksulaops.com>
2018-04-09 15:55:41 +0530

    expiry changes

--- app/code/local/Amasty/Oaction/Helper/Data.php
+++ app/code/local/Amasty/Oaction/Helper/Data.php
@@ -90,7 +90,7 @@ class Amasty_Oaction_Helper_Data extends Mage_Core_Helper_Abstract
 					            <td align="center" valign="middle" style="border-left:1px solid #aeaeae; border-bottom:1px solid #aeaeae; font-family:Trebuchet MS, Arial, Helvetica, sans-serif; font-size:12px; color:#333333; padding:20px 10px; border-right:1px solid #aeaeae;">'.$formattedPrice.'</td>
 					          </tr>';
 			}
-		}
+		}	
 		return $html;
 	}
 	public function getShippingHtml($order)
