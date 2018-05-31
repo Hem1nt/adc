84e20ae8992b4232292f9caf54144c970bec8f80
ankitIksula <ankit.a@iksulaops.com>
2018-05-31 20:37:39 +0530

    wishlist issue resolved

--- app/code/local/Magecomp/Recaptcha/Model/Observer.php
+++ app/code/local/Magecomp/Recaptcha/Model/Observer.php
@@ -59,6 +59,25 @@ class Magecomp_Recaptcha_Model_Observer
             exit;
         endif;
     }
+
+    public function Savewishlist(){
+        $g_response=Mage::app()->getRequest()->getParam('g-recaptcha-response');
+        if(isset($g_response) && !empty($g_response)):
+            if (!(Mage::helper('recaptcha')->Validate_captcha($g_response))):
+                Mage::getSingleton('core/session')->addError('Please click on the reCAPTCHA box');
+                $url = Mage::helper('core/http')->getHttpReferer() ? Mage::helper('core/http')->getHttpReferer()  : Mage::getUrl();
+                Mage::app()->getFrontController()->getResponse()->setRedirect($url);
+                Mage::app()->getResponse()->sendResponse();
+                exit;
+            endif;
+        else:
+            Mage::getSingleton('core/session')->addError('Please click on the reCAPTCHA box.');
+            $url = Mage::helper('core/http')->getHttpReferer() ? Mage::helper('core/http')->getHttpReferer()  : Mage::getUrl();
+            Mage::app()->getFrontController()->getResponse()->setRedirect($url);
+            Mage::app()->getResponse()->sendResponse();
+            exit;
+        endif;
+    }
 }
 
 
