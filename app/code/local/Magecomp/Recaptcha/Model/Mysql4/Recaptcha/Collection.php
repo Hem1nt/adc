74e7e25e8efc82d3877a397ede597c59a9d76855
ankitIksula <33057250+ankitIksula@users.noreply.github.com>
2018-06-01 22:55:55 +0530

    Revert "Ankit/google recaptcha"

--- app/code/local/Magecomp/Recaptcha/Model/Mysql4/Recaptcha/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Magecomp_Recaptcha_Model_Mysql4_Recaptcha_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
-{
-	public function _construct()
-	{
-		parent::_construct();
-		$this->_init('recaptcha/recaptcha');	
-	}
-}
