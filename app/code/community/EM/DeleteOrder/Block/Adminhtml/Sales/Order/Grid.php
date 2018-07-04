24bea432f5ccd8e695244188a00591472563a023
hemant-r <hemant.r@iksula.com>
2018-07-04 19:07:42 +0530

    text changes

--- app/code/community/EM/DeleteOrder/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/community/EM/DeleteOrder/Block/Adminhtml/Sales/Order/Grid.php
@@ -53,7 +53,7 @@ class EM_DeleteOrder_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Blo
         ));
 
         $this->addColumn('echeck_transactionid', array(
-            'header' => Mage::helper('sales')->__('Echeck Transactionid'),
+            'header' => Mage::helper('sales')->__('ECheck ID'),
             'index' => 'echeck_transactionid',
             'filter_index' => 'echeck_transactionid',
         ));
