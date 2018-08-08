4490586d71f4a3a61e03bad08131dcaf814d8b2d
preetiiksula <preeti.m@iksula.com>
2018-08-08 18:48:02 +0530

    helper changes

--- app/code/local/Iksula/Clicktoform/Helper/Data.php
+++ app/code/local/Iksula/Clicktoform/Helper/Data.php
@@ -6,26 +6,26 @@ class Iksula_Clicktoform_Helper_Data extends Mage_Core_Helper_Abstract
 		$customerEmail = $model->getCustomerEmail();
 		$customerName = $model->getCustomerName();
 		$senderName = Mage::getStoreConfig('trans_email/ident_support/name');
-		//$senderEmail = Mage::getStoreConfig('clicktoform/clicktoform_setting/clicktoform_adminemailaddress');
-		$senderEmail = Mage::getStoreConfig('trans_email/ident_support/email');
+		$senderEmail = Mage::getStoreConfig('clicktoform/clicktoform_setting/clicktoform_adminemailaddress');
+		//$senderEmail = Mage::getStoreConfig('trans_email/ident_support/email');
 		$sender = array(
 			'name' => $senderName,
 			'email' => $senderEmail
 		);
-			// Set recepient information
+		// Set recepient information
 		$recepientEmail = $customerEmail;
 		$recepientName = $customerName;
-		$templateAdmin = Mage::getStoreConfig('clicktoform/clicktoform_setting/clicktoform_settings');
+		$templateAdmin = Mage::getStoreConfig('clicktoform/clicktoform_setting/clicktoform_adminemailtemplate');
 		$templateCustomer = Mage::getStoreConfig('clicktoform/clicktoform_setting/clicktoform_customeremail');
-			// Get Store ID
+		// Get Store ID
 		$storeId = Mage::app()->getStore()->getId();
-			// Set variables that can be used in email template
+		// Set variables that can be used in email template
 		$vars = array('customername' => $model->getCustomerName(),
 			'customermoblieno' =>$model->getCustomerMoblieno(),
 			'customertime' =>$model->getCustomerTime(),
 			'customeremail'=>$model->getCustomerEmail());
 		$translate  = Mage::getSingleton('core/translate');
-			// Send Transactional Email
+		// Send Transactional Email
 		Mage::getModel('core/email_template')->sendTransactional($templateAdmin, $sender, $senderEmail, 'Admin', $vars, $storeId);
 		Mage::getModel('core/email_template')
 		->sendTransactional($templateCustomer, $sender, $recepientEmail, $recepientName, $vars, $storeId);
